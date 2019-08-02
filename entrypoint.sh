49f2d8439961faa3bb41c53a532be00d6534b2e6
Ivan Mc Kinley <i.mj.mckinley@gmail.com>
2019-08-02 12:28:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,23 @@
-while true;       do       awk -v RS= '{$1=$1}1' demo.json  >> /tmp/mylog.json;            i=$((i+1));        sleep 1; done ;
+ips=("172.123.11.1" "12.45.234.11" "123.98.4.12" "98.56.123.2" "145.78.22.1")
+httpstatus=("400" "403" "200" "202" "500" "403" "401" "500" "101" "301" "405" "505" "201" "202")
+while true;       do
+RANDOM=$$$(date +%s)
+cp demo.json temp.json
+ip=${ips[$RANDOM % ${#ips[@]}]}
+echo $ip
+sed -i "s/@ip@/$ip/g" temp.json
+RANDOM=$$$(date +%s)
+ip=${ips[$RANDOM % ${#ips[@]}]}
+sed -i "s/@clientip@/$clientip/g" temp.json
+RANDOM=$$$(date +%s)
+ip=${ips[$RANDOM % ${#ips[@]}]}
+sed -i "s/@targetip@/$targetip/g" temp.json
+RANDOM=$$$(date +%s)
+status=${httpstatus[$RANDOM % ${#httpstatus[@]}]}
+sed -i "s/@httpstatus@/$status/g" temp.json
+
+
+size=$(( ( RANDOM % 1000 )  + 1 ))
+sed -i "s/@response@/$size/g" temp.json
+
+awk -v RS= '{$1=$1}1' temp.json  >> /tmp/mylog.json;            i=$((i+1));        sleep 1; done ;
