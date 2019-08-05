6e19f00f9a47ed52c50b0e2bceb6ca88b5a51227
ivanthelad <ivanthelad>
2019-08-05 18:49:48 +0200

    s

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM alpine 
 ## file path to insepct 
+#RUN mkdir /opt/demo
+RUN  apk add --no-cache bash gawk sed grep bc coreutils
 RUN mkdir /opt/demo
 RUN addgroup -S demo && adduser -S demo -G demo
 WORKDIR  /opt/demo
 COPY demo.json /opt/demo
 COPY entrypoint.sh /opt/demo
 RUN chmod 775  -R /opt/demo
+RUN chown demo:demo /opt/demo 
 user demo
-ENTRYPOINT  ["sh", "/opt/demo/entrypoint.sh"] 
+
+ENTRYPOINT  ["/bin/bash", "/opt/demo/entrypoint.sh"] 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,20 @@
+#!/bin/bash
+
 ips=("172.123.11.1" "12.45.234.11" "123.98.4.12" "98.56.123.2" "145.78.22.1")
+
 httpstatus=("400" "403" "200" "202" "500" "403" "401" "500" "101" "301" "405" "505" "201" "202")
-while true;       do
+while true
+  do
 RANDOM=$$$(date +%s)
 cp demo.json temp.json
 ip=${ips[$RANDOM % ${#ips[@]}]}
-sed -i "s/@ip@/$ip/g" temp.json
+sed -i 's/@ip@/$ip/g' temp.json
 RANDOM=$$$(date +%s)
 ip=${ips[$RANDOM % ${#ips[@]}]}
 sed -i "s/@clientip@/$clientip/g" temp.json
 RANDOM=$$$(date +%s)
 ip=${ips[$RANDOM % ${#ips[@]}]}
-sed -i "s/@targetip@/$targetip/g" temp.json
+sed -i  "s/@targetip@/$targetip/g" temp.json
 RANDOM=$$$(date +%s)
 status=${httpstatus[$RANDOM % ${#httpstatus[@]}]}
 sed -i "s/@httpstatus@/$status/g" temp.json
@@ -18,5 +22,6 @@ sed -i "s/@httpstatus@/$status/g" temp.json
 
 size=$(( ( RANDOM % 1000 )  + 1 ))
 sed -i "s/@response@/$size/g" temp.json
-
-awk -v RS= '{$1=$1}1' temp.json  >> /tmp/mylog.json;            i=$((i+1));     1>&2 echo $(cat temp.json)   sleep 1; done ;
+awk -v RS= '{$1=$1}1' temp.json  >> /tmp/mylog.json;            i=$((i+1));     1>&2 echo $(cat temp.json)   
+sleep 1
+ done 
